eb5a1aaf2fa1086d5558ce485de9c49b377dec3d
Simone <simonepell03@gmail.com>
2025-03-31 18:16:32 +0200

    Rimuovi build dal versionamento

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/simone/Documents/Università/LabProgrammazione/Transactions
-# Build directory: /home/simone/Documents/Università/LabProgrammazione/Transactions/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(RunAllTests "/home/simone/Documents/Università/LabProgrammazione/Transactions/build/runTests")
-set_tests_properties(RunAllTests PROPERTIES  _BACKTRACE_TRIPLES "/home/simone/Documents/Università/LabProgrammazione/Transactions/CMakeLists.txt;46;add_test;/home/simone/Documents/Università/LabProgrammazione/Transactions/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
